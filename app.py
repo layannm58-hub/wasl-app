9e0050d9aaef1371ec2d6972160074ffe1c8b3ae
layannm58-hub <layann.m58@gmail.com>
2026-09-07 00:57:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ st.markdown("""
         padding: 6px 24px;
         display: inline-block;
     }
-    .top-left-box { min-height: 48vh; }
+    .top-left-box { min-height: 55vh; }
     .bottom-right-box { min-height: 30vh; }
     </style>
 """, unsafe_allow_html=True)
